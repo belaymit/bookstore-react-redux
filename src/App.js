3140d235c71c2473b9a2ed4b04ee9a9ebcc27ab4
belaymit <2belamit@gmail.com>
2023-01-20 17:00:35 +0300

    added Routing to Categories component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import Categories from './components/categories/Categories';
 import Home from './pages/home/Home';
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
       <Footer />
     </BrowserRouter>
