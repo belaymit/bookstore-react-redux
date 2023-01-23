a7248a1d76bb3bcab6939813767771be9de6abda
belaymit <2belamit@gmail.com>
2023-01-23 20:46:09 +0300

    added routing to Store and Contacts pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Categories from './components/categories/Categories';
 import Home from './pages/home/Home';
+import Store from './pages/store/Store';
+import BookDetails from './components/bookDetails/BookDetails';
+import Contacts from './pages/contacts/Contacts';
 
 const App = () => (
   <>
@@ -12,6 +15,9 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/store/:id" element={<BookDetails />} />
       </Routes>
       <Footer />
     </BrowserRouter>
