e4ef6e6a8eb227da48b729b20d59d8cafe20117a
belaymit <2belamit@gmail.com>
2023-01-23 22:38:05 +0300

    imported store from configure store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.scss';
-import store from './redux/store';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
