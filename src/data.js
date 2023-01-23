e29730f526babac3bc7a5faa7c6e59d7def705fd
belaymit <2belamit@gmail.com>
2023-01-23 20:50:43 +0300

    added dummy data for testing and removed unnecessary files

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id: 1, title: 'Born Crime', author: 'Trover Noha', category: 'Fiction',
+  },
+  {
+    id: 2, title: 'After Many a Summer Dies the Swan', author: 'Aldous Huxley', category: 'Geography',
+  },
+  {
+    id: 3, title: 'All Passion Spent', author: 'Vita Sackville-West', category: 'Politics',
+  },
+  {
+    id: 4, title: 'Alone on a Wide, Wide Sea', author: 'Michael Morpurgo', category: 'Fiction',
+  },
+  {
+    id: 5, title: 'Beyond the Mexique Bay', author: 'Aldous Huxley', category: 'Education',
+  },
+];
+
+export default data;
