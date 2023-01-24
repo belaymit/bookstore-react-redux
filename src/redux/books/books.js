1a3ea8b1ddd9d71f18a986ea462f5e4d828663d9
belaymit <2belamit@gmail.com>
2023-01-24 15:05:10 +0300

    added GET_ALL_BOOKS as action creator

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,21 +1,17 @@
+import apis from '../../booksApis/apis';
+
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 const GET_ALL_BOOKS = 'GET_ALL_BOOKS';
-const initialState = [
-  {
-    id: 1, title: 'Born Crime', author: 'Trover Noha', category: 'Fiction',
-  },
-  {
-    id: 2, title: 'After Many a Summer Dies the Swan', author: 'Aldous Huxley', category: 'Geography',
-  },
-];
+
+const initialState = [];
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.payload];
+      return [...state, action.book];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.id !== action.book);
     case GET_ALL_BOOKS:
       return action.books;
     default:
@@ -23,14 +19,28 @@ const booksReducer = (state = initialState, action) => {
   }
 };
 
-export const addBooks = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
-});
+export const addBooks = (book) => async (dispatch) => {
+  apis.addBooks(book);
+  dispatch({
+    type: ADD_BOOK,
+    book,
+  });
+};
 
-export const removeBook = (book) => ({
-  type: REMOVE_BOOK,
-  payload: book,
-});
+export const removeBook = (book) => async (dispatch) => {
+  apis.removeBook(book);
+  dispatch({
+    type: REMOVE_BOOK,
+    book,
+  });
+};
+
+export const fetchBooks = () => async (dispatch) => {
+  const books = await apis.fetchBooks();
+  dispatch({
+    type: GET_ALL_BOOKS,
+    books,
+  });
+};
 
 export default booksReducer;
