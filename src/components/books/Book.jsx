5666b4be0e41dedc8667a651ac6709651b4d9020
belaymit <2belamit@gmail.com>
2023-01-24 08:00:33 +0300

    changed some classes and structure

--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -1,49 +1,46 @@
 import React from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
 import PropTypes from 'prop-types';
-import '../../styles/books.scss';
-import 'react-circular-progressbar/dist/styles.css';
+import progress from '../../assets/progress.png';
+import '../../styles/books.css';
 
 const Book = (props) => {
   const { data } = props;
   return (
-    <div className="book-container">
-      <div className="book-info">
-        <div className="book-description">
-          <h5>{data.category}</h5>
+    <li key={data.id} className="book">
+      <div className="book-content">
+        <div className="book-info">
+          <h4>{data.category}</h4>
           <h2>{data.title}</h2>
-          <h4>{data.author}</h4>
+          <h6>{data.author}</h6>
+          <div className="action-buttons">
+            <button type="button" className="comment">Comments</button>
+            <div className="vertical-divider" />
+            <button
+              type="button"
+            >
+              Remove
+            </button>
+            <div className="vertical-divider" />
+            <button type="button" className="edit">Edit</button>
+          </div>
         </div>
-        <div className="action-btn">
-          <button type="button">
-            Comments
-          </button>
-          <div className="separator" />
-          <button type="button">
-            Remove
-          </button>
-          <div className="separator" />
-          <button type="button">
-            Edit
-          </button>
-        </div>
-      </div>
-      <div className="progress">
-        <div className="progress-bar">
-          <CircularProgressbar value={70} strokeWidth={10} />
-        </div>
-        <div className="progress-description">
-          <p className="percent-complete">70%</p>
-          <p className="completed">Completed</p>
-        </div>
-        <div className="progress-divider" />
-        <div className="chapter-container">
-          <p>Current Chapter</p>
-          <h4>Chapter 17</h4>
-          <button type="submit" className="btn-submit">UPDATE PROGRESS</button>
+        <div className="book-progress">
+          <div className="progress-bar">
+            <img src={progress} alt="progress" />
+          </div>
+          <div className="progress-info">
+            <p className="percent-complete">64%</p>
+            <p className="completed">Completed</p>
+          </div>
+          <div className="progress-divider" />
+          <div className="chapter-container">
+            <p>Current Chapter</p>
+            <h4>Chapter 17</h4>
+            <button type="submit">UPDATE PROGRESS</button>
+          </div>
         </div>
       </div>
-    </div>
+    </li>
   );
 };
 
